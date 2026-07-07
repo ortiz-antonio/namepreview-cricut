631edfa2fc0a799e98bb7303b90b9c204678acf2
Antonio Ortiz <ortiz.antonio@gmail.com>
2026-07-07 10:06:26 -0500

    refactor: cutting files

--- core/src/content/types.ts
+++ core/src/content/types.ts
@@ -5,6 +5,9 @@ export interface HeroData {
   text2: string
   text3: string
   image_kenzie: string
+  image_kenzie_cut: string
   image_koryn: string
+  image_koryn_cut: string
   image_sofia: string
+  image_sofia_cut: string
 }
